1a3c9eeab7f94ce349fac9165d206cb6f044eb34
Jakub Tarchalski <jakub.tarchalski@gmail.com>
2023-07-07 00:09:54 +0100

    凸༼ຈل͜ຈ༽凸 kinematics

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,24 @@ struct Object {
     mass: i32,
     position: Position,
     velocity: Velocity,
+    acceleration: Acceleration,
     momentum: Momentum,
 }
 
 impl Object {
-    fn update_velocity(&mut self, acceleration: f64) {
-        self.velocity.2 = self.velocity.2 + acceleration;
+    // v = u + at
+    fn update_velocity(&mut self) {
+        self.velocity.2 = self.velocity.2 + self.acceleration.2;
     }
 
+    // p = mv
     fn update_momentum(&mut self) {
         self.momentum.2 = (self.mass as f64) * self.velocity.2;
     }
 
+    // x = x + vt
     fn update_position(&mut self) {
-        self.position.2 = self.position.2 + self.velocity.2
+        self.position.2 = self.position.2 + self.velocity.2 + (self.acceleration.2 / 2.0);
     }
 }
 
@@ -33,26 +37,32 @@ fn main() {
     let mut frame: i32 = 0;
     let gravity: f64 = -9.81;
     let max_frames: i32 = 6;
-    let max_down_velocity: f64 = -30.0;
+    let max_down_velocity: f64 = -29.43;
 
     let mut particle = Object {
         mass: 9,
         position: Position(0.0, 0.0, 0.0),
         velocity: Velocity(0.0, 0.0, 0.0),
+        acceleration: Acceleration(0.0, 0.0, 0.0),
         momentum: Momentum(0.0, 0.0, 0.0),
     };
+
+    particle.acceleration.2 = gravity;
 
     // do something
     while frame <= max_frames {
         dbg!(&particle);
         //println!("frame: {}, velocity: {}, momentum: {}, z position: {}", frame, particle.velocity, particle.momentum, particle.position.2);
 
-        if (particle.velocity.2 + gravity) > max_down_velocity {
-            particle.update_velocity(gravity);
+        particle.update_position();
+
+        if particle.velocity.2 <= max_down_velocity {
+            particle.acceleration.2 = 0.0;
+        } else {
+            particle.update_velocity();
         }
 
         particle.update_momentum();
-        particle.update_position();
 
         frame += 1;
     }
@@ -69,5 +79,6 @@ fn main() {
 // gravity
 // mass
 
-// f=ma
-// v=d/t
+// f = ma
+// v = x/t
+// s = vt + (a/2)(t^2)
